22948fefddf3b715af5105340418c00eea40606a
Eduardo Quirino <edu.3532@gmail.com>
2022-02-01 14:09:51 -0300

    Atualização tamanho do formulário

--- ConsultarCEPs/FrmConsultarCEPs.Designer.cs
+++ ConsultarCEPs/FrmConsultarCEPs.Designer.cs
@@ -31,14 +31,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtCEP = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtEstado = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtCidade = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtBairro = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.txtRua = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnConsultar = new System.Windows.Forms.Button();
             this.btnLimpar = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
@@ -48,18 +48,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 24);
+            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "CEP:";
             // 
             // txtCEP
             // 
             this.txtCEP.BackColor = System.Drawing.SystemColors.Info;
-            this.txtCEP.Location = new System.Drawing.Point(23, 44);
+            this.txtCEP.Location = new System.Drawing.Point(17, 36);
+            this.txtCEP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCEP.Name = "txtCEP";
-            this.txtCEP.Size = new System.Drawing.Size(156, 22);
+            this.txtCEP.Size = new System.Drawing.Size(118, 20);
             this.txtCEP.TabIndex = 1;
             // 
             // groupBox1
@@ -72,88 +74,99 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.txtEstado);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(23, 96);
+            this.groupBox1.Location = new System.Drawing.Point(17, 78);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(541, 259);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(682, 265);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Resultado";
             // 
-            // txtEstado
-            // 
-            this.txtEstado.BackColor = System.Drawing.Color.LightGray;
-            this.txtEstado.Location = new System.Drawing.Point(22, 51);
-            this.txtEstado.Name = "txtEstado";
-            this.txtEstado.Size = new System.Drawing.Size(497, 22);
-            this.txtEstado.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Estado:";
-            // 
-            // txtCidade
-            // 
-            this.txtCidade.BackColor = System.Drawing.Color.LightGray;
-            this.txtCidade.Location = new System.Drawing.Point(22, 107);
-            this.txtCidade.Name = "txtCidade";
-            this.txtCidade.Size = new System.Drawing.Size(497, 22);
-            this.txtCidade.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 87);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 17);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Cidade:";
-            // 
-            // txtBairro
-            // 
-            this.txtBairro.BackColor = System.Drawing.Color.LightGray;
-            this.txtBairro.Location = new System.Drawing.Point(23, 160);
-            this.txtBairro.Name = "txtBairro";
-            this.txtBairro.Size = new System.Drawing.Size(497, 22);
-            this.txtBairro.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 140);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 17);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Bairro:";
-            // 
             // txtRua
             // 
             this.txtRua.BackColor = System.Drawing.Color.LightGray;
-            this.txtRua.Location = new System.Drawing.Point(23, 217);
+            this.txtRua.Location = new System.Drawing.Point(17, 176);
+            this.txtRua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtRua.Name = "txtRua";
-            this.txtRua.Size = new System.Drawing.Size(497, 22);
+            this.txtRua.Size = new System.Drawing.Size(624, 20);
             this.txtRua.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(20, 197);
+            this.label5.Location = new System.Drawing.Point(15, 160);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.Size = new System.Drawing.Size(30, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = "Rua:";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.BackColor = System.Drawing.Color.LightGray;
+            this.txtBairro.Location = new System.Drawing.Point(17, 130);
+            this.txtBairro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(624, 20);
+            this.txtBairro.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 114);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Bairro:";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.BackColor = System.Drawing.Color.LightGray;
+            this.txtCidade.Location = new System.Drawing.Point(16, 87);
+            this.txtCidade.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(624, 20);
+            this.txtCidade.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 71);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cidade:";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.BackColor = System.Drawing.Color.LightGray;
+            this.txtEstado.Location = new System.Drawing.Point(16, 41);
+            this.txtEstado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(624, 20);
+            this.txtEstado.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Estado:";
             // 
             // btnConsultar
             // 
             this.btnConsultar.BackgroundImage = global::ConsultarCEPs.Properties.Resources.busca_cep;
             this.btnConsultar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnConsultar.Location = new System.Drawing.Point(337, 15);
+            this.btnConsultar.Location = new System.Drawing.Point(417, 20);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(227, 65);
+            this.btnConsultar.Size = new System.Drawing.Size(282, 66);
             this.btnConsultar.TabIndex = 3;
             this.btnConsultar.UseVisualStyleBackColor = true;
             this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
@@ -162,9 +175,10 @@
             // 
             this.btnLimpar.Image = global::ConsultarCEPs.Properties.Resources.limpar;
             this.btnLimpar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnLimpar.Location = new System.Drawing.Point(322, 370);
+            this.btnLimpar.Location = new System.Drawing.Point(518, 361);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(112, 33);
+            this.btnLimpar.Size = new System.Drawing.Size(90, 40);
             this.btnLimpar.TabIndex = 4;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
@@ -174,9 +188,10 @@
             // 
             this.btnSair.Image = global::ConsultarCEPs.Properties.Resources.sair;
             this.btnSair.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSair.Location = new System.Drawing.Point(452, 370);
+            this.btnSair.Location = new System.Drawing.Point(615, 361);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSair.Name = "btnSair";
-            this.btnSair.Size = new System.Drawing.Size(112, 33);
+            this.btnSair.Size = new System.Drawing.Size(90, 40);
             this.btnSair.TabIndex = 5;
             this.btnSair.Text = "Sair";
             this.btnSair.UseVisualStyleBackColor = true;
@@ -184,9 +199,9 @@
             // 
             // FrmConsultarCEPs
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 413);
+            this.ClientSize = new System.Drawing.Size(742, 416);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnConsultar);
@@ -194,6 +209,7 @@
             this.Controls.Add(this.txtCEP);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "FrmConsultarCEPs";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Consultar CEPs
 
+![image-20220201140711343](C:\Users\Eduardo\AppData\Roaming\Typora\typora-user-images\image-20220201140711343.png)
+
   - App criado para consultar cep, usado uma API dos correios para elaboração do projeto.
+
+![image-20220201140623895](C:\Users\Eduardo\AppData\Roaming\Typora\typora-user-images\image-20220201140623895.png)
